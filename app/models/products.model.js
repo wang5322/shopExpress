65ff9701b443475e4f2c9d70bc2d50196df6b7be
ShiyuanX <124700939+ShiyuanX@users.noreply.github.com>
2023-06-07 12:52:40 -0400

    modified

--- app/models/products.model.js
+++ app/models/products.model.js
@@ -49,7 +49,7 @@ Products.getAll = (category, searchFor, available, result) => {
         filterStr += `(productCode like '%${searchFor}%' or productName like '%${searchFor}%' or productDesc like '%${searchFor}%')`;
     }
     filterStr += `(productCode like '%${searchFor}%' or productName like '%${searchFor}%' or productDesc like '%${searchFor}%')`;
-  }
+
   if (filterStr.length > 0) {
     queryStr += ` where ${filterStr}`;
   }
