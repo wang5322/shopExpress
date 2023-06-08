92a43a9b3f6fc19439bdef881a75c48c611b4d8c
ShiyuCCC <115126635+shiyuCCC@users.noreply.github.com>
2023-06-08 09:31:10 -0400

    update

--- static/javascript/inventory.js
+++ static/javascript/inventory.js
@@ -1,19 +1,33 @@
 var available = 1;
-var sellerId = 1;
+var sellerId = 20;
 
 $(document).ready(function () {
-  window.onload = (event) => {
-    console.log("page is fully loaded");
-    refreshInventoryList();
-  };
+  console.log("page is fully loaded");
+  refreshInventoryList();
+  $(".alert").hide(); // error message alert
+
+  $("#add").on("click", function () {
+    $(".alert").show();
+    // $(".close").alert("close");
+  });
 });
 
 function refreshInventoryList() {
   $.ajax({
-    url: "/api/products?available",
+    url: `/api/products/?sellerId=${sellerId}`,
+    type: "GET",
+    dataType: "json",
+    success: function (response) {
+      console.log(response);
+      for (let i = 0; i < response.length; i++) {
+        var product = response[i];
+        $("#productList").html(
+          `<p> ${product.productName}</p><div class=\'card productImageCard\'><img src=\'${product.imageUrl}\'><div>`
+        );
+      }
+    },
+    error: function (error) {
+      console.log("Error: ", error);
+    },
   });
-
-
-  
-  $("inventoryList");
 }
