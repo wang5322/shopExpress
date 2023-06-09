7ae84b9f7594a0735c3a72e7d94998ce71d4df39
ShiyuanX <124700939+ShiyuanX@users.noreply.github.com>
2023-06-08 22:00:32 -0400

    post, api/carts

--- app/routes/orders.routes.js
+++ app/routes/orders.routes.js
@@ -3,6 +3,7 @@ module.exports = (app) => {
     let router = require("express").Router();
 
     router.post("/", orders.create);
+    router.get("/", orders.getTest);
     app.use('/api/orders', router);
     app.use('/api/carts', router);
 }
