c5140b1061617d14ffca48769d2a6cf634f32045
ShiyuCCC <115126635+shiyuCCC@users.noreply.github.com>
2023-06-12 15:42:37 -0400

    Update productlist.js

--- static/javascript/productlist.js
+++ static/javascript/productlist.js
@@ -7,6 +7,8 @@ $(document).ready(function () {
   // myParam2 = urlParams.get("searchFor");
   console.log(myParam);
   refreshProductList();
+
+  $;
 });
 
 function refreshProductList() {
@@ -19,7 +21,6 @@ function refreshProductList() {
     },
     success: function (response) {
       console.log("category is : " + myParam);
-      var result = "";
       console.log(response);
       // loop the info of products
       for (let i = 0; i < response.length; i++) {
@@ -35,46 +36,34 @@ function refreshProductList() {
         }
 
         // insert to the DOM
-        result +=
-          `<div class="col mb-5">
+        var result = `<div class="col mb-5">
         <div class="card h-100">
-          <img
-            class="card-img-top"
-            src="` +
-          imagepath +
-          `"
-            alt="product image"
-            style="height:200px;object-fit: cover; object-position: center;"
-          />
-      
+          <img class="card-img-top" src="${imagepath}" alt="product image" style="height:200px;object-fit: cover; object-position: center;" />
           <div class="card-body p-4">
             <div class="text-center">
-              <h5 class="fw-bolder">` +
-          product.productName +
-          `</h5>
-      
-              <div id="price">$` +
-          product.price +
-          `</div>
+              <h5 class="fw-bolder">${product.productName}</h5>
+              <div id="price">$${product.price}</div>
             </div>
           </div>
-      
           <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
             <div class="text-center">
-              <a class="btn btn-outline-dark mt-auto" href="productdetail.html?productId=` +
-          product.id +
-          `">
+              <a class="btn btn-outline-dark mt-auto" href="productdetail.html?productId=${product.id}">
                 View details
-              </a>
-              <button id="addToCart" class="btn btn-dark mt-1">
+              </a><select id="quantitySelect-${product.id}" class="form-control text-center me-3" style="width: 60px; display: inline;">
+              </select><button id="addToCart-${product.id}" class="btn btn-dark mt-1">
                 Add to cart
               </button>
             </div>
           </div>
         </div>
       </div>`;
+
+        $("#productListPane").append(result);
+
+        for (let i = 1; i <= 30; i++) {
+          $(`#quantitySelect-${product.id}`).append(new Option(i, i));
+        }
       }
-      $("#productListPane").html(result);
     },
   });
 }
