c55bfc8b630aa36a354ca27610a63e159a8bf617
ShiyuCCC <115126635+shiyuCCC@users.noreply.github.com>
2023-06-11 23:16:12 -0400

    orderhistory getorders

--- app/routes/orders.routes.js
+++ app/routes/orders.routes.js
@@ -1,17 +1,16 @@
 module.exports = (app) => {
-    const orders = require("../controllers/orders.controller");
-    let router = require("express").Router();
+  const orders = require("../controllers/orders.controller");
+  let router = require("express").Router();
 
-    router.post("/", orders.create);
-    router.put("/", orders.getAll);
-    router.get("/:id([0-9]+)", orders.getOne);
-    router.put("/:id([0-9]+)", orders.buyerConfirm);
-    router.delete("/:id([0-9]+)", orders.delete);
+  router.post("/", orders.create);
+  router.get("/", orders.getAll);
+  router.get("/:id([0-9]+)", orders.getOne);
+  router.put("/:id([0-9]+)", orders.buyerConfirm);
+  router.delete("/:id([0-9]+)", orders.delete);
 
-    router.patch("/pay/:id([0-9]+)", orders.payOrder);
+  router.patch("/pay/:id([0-9]+)", orders.payOrder);
 
-    router.patch("/:id([0-9]+)", orders.modifyStatus);
+  router.patch("/:id([0-9]+)", orders.modifyStatus);
 
-    app.use('/api/orders', router);
-
-}
+  app.use("/api/orders", router);
+};
