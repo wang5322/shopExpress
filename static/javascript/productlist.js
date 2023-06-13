5fa8531ae63318d2efd1318b007a5e3e15afbd51
ShiyuCCC <115126635+shiyuCCC@users.noreply.github.com>
2023-06-12 23:16:29 -0400

    add to cart

--- static/javascript/productlist.js
+++ static/javascript/productlist.js
@@ -62,7 +62,7 @@ $(document).ready(function () {
                 <a class="btn btn-outline-dark mt-auto" href="productdetail.html?productId=${product.id}">
                   View details
                 </a><select id="quantitySelect-${product.id}" class="form-control text-center me-3" style="width: 60px; display: inline;">
-                </select><button id="addToCart-${product.id}" class="btn btn-dark mt-1">
+                </select><button id="addToCart-${product.id}" data-product-id="${product.id}" class="btn btn-dark mt-1">
                   Add to cart
                 </button>
               </div>
@@ -79,7 +79,14 @@ $(document).ready(function () {
           // add events to addToCart button
           $(`#addToCart-${product.id}`).click(function () {
             var selectedQuantity = $(`#quantitySelect-${product.id}`).val();
-            console.log("Selected Quantity: ", selectedQuantity);
+
+            console.log(
+              "Product ID",
+              product.id,
+              "Selected Quantity: ",
+              selectedQuantity
+            );
+            var productId = $(this).data("product-id");
             $.ajax({
               url: `/api/carts`,
               type: "POST",
@@ -90,7 +97,7 @@ $(document).ready(function () {
               },
               data: {
                 amount: selectedQuantity,
-                id: product.id,
+                id: productId,
               },
               success: function (response) {
                 $("#myModalBody").html("Product added to cart successfully!");
@@ -98,7 +105,9 @@ $(document).ready(function () {
                 console.log("Product added to cart successfully!");
               },
               error: function (error) {
-                $("#myModalBody").html("Error adding product to cart");
+                $("#myModalBody").html(
+                  "Error adding product to cart. Please login first!"
+                );
                 $("#myModal").modal("show");
                 console.log("Error adding product to cart:", error);
               },
